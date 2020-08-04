26eb332981f3c6c5f96e2522393b3470b8a18b86
cheptegei-create <davidchepte@gmail.com>
2020-08-04 14:06:03 +0300

    added files and attributes

--- hello.py
+++ hello.py
@@ -1,2 +1,15 @@
-msg = "hello world"
-print(msg)
+#msg = "hello world"
+#print(msg)
+#print("o----")
+#print(" ||||")
+#print('*' * 10)
+#price = 10
+#print(price)
+#name = "John Smith" 
+#age = 20
+#is_new = True 
+#print(name)
+#print(age)
+#print(is_new)
+name = input("what is your name ")
+#print("Hi " + name)

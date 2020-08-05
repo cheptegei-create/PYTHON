ebd093dbd3b26a21e6bc6e9ad41f45114aeb7e47
cheptegei-create <davidchepte@gmail.com>
2020-08-05 07:51:36 +0300

    python practice

--- hello.py
+++ hello.py
@@ -11,5 +11,25 @@
 #print(name)
 #print(age)
 #print(is_new)
-name = input("what is your name ")
+#name = input("what is your name? ")
 #print("Hi " + name)
+#favourite_colour = input("what is your favourite colour? ")
+#print(name + " likes " + favourite_colour)
+#birth_year = input("birth year: ")
+#age = 2020 - int(birth_year)
+#print(age)
+#weight_lbs = float(input("what is your weight in pounds: "))
+#weight_kilograms = weight_lbs * 0.45
+#print(weight_kilograms)
+#name = "John Smith"
+#print(name[1:-1])
+#email = '''
+#Hello, 
+#I wish to request for an entry-level position.
+#'''
+#print(email)
+first = 'John'
+last = 'Smith'
+#msg = first + ' [' + last + '] is a coder' 
+msg = f'{first} [{last}] is a coder'
+print(msg)
